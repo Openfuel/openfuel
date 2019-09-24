fd1cabe331fe09a27badf90457a7c6a506c50d35
DanCodes <dan@dancodes.online>
2019-09-24 16:49:31 +0200

    add passport

--- utils/handlers/github.js
+++ utils/handlers/github.js
@@ -1,12 +1,16 @@
-var GitHubStrategy = require('passport-github').Strategy;
-var User = require('../models/user');
+const GitHubStrategy = require("passport-github").Strategy;
+const passport = require("passport");
+const User = require("../models/user");
 
-passport.use(new GitHubStrategy({
-    clientID: process.env.GITHUB_CLIENT_ID,
-    clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: "http://localhost:8080/auth/github/callback"
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    // ToDo
-  }
-));
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: "http://localhost:8080/auth/github/callback"
+    },
+    function(accessToken, refreshToken, profile, cb) {
+      // ToDo
+    }
+  )
+);
