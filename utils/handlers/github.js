d5453f32a6ab2169ce6622d3a823bc2c2987552a
DanCodes <dan@dancodes.online>
2019-09-24 16:51:22 +0200

    add option for testing

--- utils/handlers/github.js
+++ utils/handlers/github.js
@@ -5,8 +5,8 @@ const User = require("../models/user");
 passport.use(
   new GitHubStrategy(
     {
-      clientID: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      clientID: process.env.GITHUB_CLIENT_ID || " ",
+      clientSecret: process.env.GITHUB_CLIENT_SECRET || " ",
       callbackURL: "http://localhost:8080/auth/github/callback"
     },
     function(accessToken, refreshToken, profile, cb) {
