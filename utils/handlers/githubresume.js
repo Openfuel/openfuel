71a68d22cb0b95346825ad4b6d4053a779af3d34
Divy Srivastava <dj.srivastava23@gmail.com>
2019-09-26 16:56:34 +0530

    Cleanup

--- utils/handlers/githubresume.js
+++ utils/handlers/githubresume.js
@@ -1,6 +1,4 @@
-var urlParams = {};
-// We don't need this --> var username;
-var trackerId = 'UA-21222559-1';
+var axois = require('axios');
 
 module.exports = (username) => {};
 
@@ -558,17 +556,3 @@ var run = function() {
     });
 
 };
-
-if (trackerId) {
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', trackerId]);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-}
-
-$(window).bind('error', error);
