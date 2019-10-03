909cfddabbbe2376fe9866eb1e811949d80bb98e
DanCodes <dan@dancodes.online>
2019-10-03 12:29:36 +0200

    Revert "fix user search"

--- utils/handlers/user.js
+++ utils/handlers/user.js
@@ -162,7 +162,7 @@ function comment(user, comment, _id, cb) {
         obj.posts[i].comments.push(comment);
         obj.notifications.push({
           msg: `@${comment.by} reacted to your post.`,
-          link: `/u/@${data[i].username}`,
+          link: `/u/${comment.by}`,
           time: new Date()
         });
         obj = new User(obj);
@@ -182,7 +182,7 @@ function like(user, like, _id, cb) {
         obj.posts[i].likes.push(like.by);
         obj.notifications.push({
           msg: `@${like.by} liked your post.`,
-          link: `/u/@${data[i].username}`,
+          link: `/u/${like.by}`,
           time: new Date()
         });
         obj = new User(obj);
